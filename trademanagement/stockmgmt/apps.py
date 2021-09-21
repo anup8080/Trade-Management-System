30547e1fad7fef16f5e768e24140fdc0b53b2a11
anup8080 <69760695+anup8080@users.noreply.github.com>
2021-09-21 20:28:02 +0545

    Add files via upload

---
+++ trademanagement/stockmgmt/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class StockmgmtConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stockmgmt'
+    #verbose_name = "trademgmt"
+    
+
