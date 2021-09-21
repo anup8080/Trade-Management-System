47634a86ac45a04b0108aec634d5cf8bc7895dde
anup8080 <69760695+anup8080@users.noreply.github.com>
2021-09-21 20:29:22 +0545

    Delete trademanagement/stockmgmt directory

--- trademanagement/stockmgmt/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class StockmgmtConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stockmgmt'
-    #verbose_name = "trademgmt"
-    
-
